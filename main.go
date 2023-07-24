1eab0e098fab4d522e433c2532bd8693b690b547
Arthur Busser <arthur.busser@gopigment.com>
2023-07-24 23:23:54 +0200

    wip

--- main.go
+++ main.go
@@ -9,6 +9,7 @@ import (
 	"path/filepath"
 	"sync"
 
+	"github.com/hashicorp/go-version"
 	flag "github.com/spf13/pflag"
 
 	"github.com/busser/tfautomv/pkg/engine"
@@ -39,27 +40,25 @@ func run() error {
 		return nil
 	}
 
-	err := smokeTests()
+	ctx := context.TODO()
+
+	/*
+	 * Step 0: Smoke tests
+	 *
+	 * Detect any obvious issues with the user's configuration.
+	 */
+
+	err := smokeTests(ctx)
 	if err != nil {
 		return err
 	}
 
-	modulePaths := flag.Args()
-	if len(modulePaths) == 0 {
-		modulePaths = []string{"."}
-	}
-
-	planOptions := []terraform.PlanOption{
-		terraform.WithTerraformBin(terraformBin),
-		terraform.WithSkipInit(skipInit),
-		terraform.WithSkipRefresh(skipRefresh),
-	}
-
-	// TODO: suppress output when --quiet is set.
-	plans, err := getPlans(context.TODO(), modulePaths, planOptions)
-	if err != nil {
-		return err
-	}
+	/*
+	 * Step 1: Parse user-provided rules
+	 *
+	 * Best to parse the rules early on, so that any syntax errors are caught
+	 * before we start running Terraform commands.
+	 */
 
 	var userRules []engine.Rule
 	for _, raw := range ignoreRules {
@@ -71,53 +70,81 @@ func run() error {
 		userRules = append(userRules, rule)
 	}
 
-	comparisons := engine.CompareAll(engine.MergePlans(plans), userRules)
+	/*
+	 * Step 2: Obtain Terraform plan
+	 *
+	 * Run `terraform plan` for each working directory provided by the user.
+	 */
+
+	workdirs := flag.Args()
+	if len(workdirs) == 0 {
+		workdirs = []string{"."}
+	}
+
+	planOptions := []terraform.Option{
+		terraform.WithTerraformBin(terraformBin),
+		terraform.WithSkipInit(skipInit),
+		terraform.WithSkipRefresh(skipRefresh),
+	}
+
+	plans, err := getPlans(ctx, workdirs, planOptions)
+	if err != nil {
+		return err
+	}
+
+	/*
+	 * Step 3: Use the tfautomv engine to determine moves to make
+	 *
+	 * We merge the plans from each working directory into a single plan
+	 * containing all the resources. This doesn't make a difference to the
+	 * engine.
+	 */
+
+	mergedPlan := engine.MergePlans(plans)
+	comparisons := engine.CompareAll(mergedPlan, userRules)
 	moves := engine.DetermineMoves(comparisons)
 
-	if !quiet {
-		summarizer := pretty.NewSummarizer(moves, comparisons, verbosity)
-		summary := summarizer.Summary()
-		os.Stderr.WriteString("\n" + summary + "\n\n")
-	}
+	/*
+	 * Step 4: Print a human-readable summary for the user
+	 *
+	 * This summary allows the user to gain insight into what tfautomv has
+	 * done, and why. This information allows the user to make an informed
+	 * decision about what to do next.
+	 */
 
-	if len(moves) == 0 {
-		return nil
-	}
+	summarizer := pretty.NewSummarizer(moves, comparisons, verbosity)
+	summary := summarizer.Summary()
+
+	os.Stderr.WriteString("\n" + summary + "\n\n")
+
+	/* Step 5: Write the moves found by the engine.
+	 *
+	 * Depending on the output format chosen by the user, we either write
+	 * the moves to a file, print them to standard output, or a combination
+	 * of both.
+	 */
 
 	terraformMoves := engineMovesToTerraformMoves(moves)
+	sameModule, differentModule := categorizeMoves(terraformMoves)
 
 	switch outputFormat {
+	case "auto":
+		if err := writeMovedBlocks(sameModule); err != nil {
+			return err
+		}
+		if err := writeMoveCommands(differentModule); err != nil {
+			return err
+		}
 	case "blocks":
-		if len(modulePaths) > 1 {
-			return fmt.Errorf("blocks output format is not supported for multiple modules")
-		}
-
-		movesFilePath := filepath.Join(modulePaths[0], "moves.tf")
-		movesFile, err := os.OpenFile(movesFilePath, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
-		if err != nil {
-			return fmt.Errorf("failed to open %q: %w", movesFilePath, err)
-		}
-
-		err = terraform.WriteMovedBlocks(movesFile, terraformMoves)
-		if err != nil {
-			return fmt.Errorf("failed to write moved blocks: %w", err)
-		}
-
-		if !quiet {
-			os.Stderr.WriteString(pretty.Colorf("%s written to [bold][green]%s", pretty.StyledNumMoves(len(moves)), movesFilePath))
-			os.Stderr.WriteString("\n")
+		if err := writeMovedBlocks(sameModule); err != nil {
+			return err
 		}
 	case "commands":
-		err := terraform.WriteMoveCommands(os.Stdout, terraformMoves)
-		if err != nil {
-			return fmt.Errorf("failed to write move commands: %w", err)
-		}
-
-		if !quiet {
-			os.Stderr.WriteString(pretty.Colorf("%s written to [bold][green]standard output", pretty.StyledNumMoves(len(moves))))
-			os.Stderr.WriteString("\n")
+		if err := writeMoveCommands(terraformMoves); err != nil {
+			return err
 		}
 	default:
+		// This should have been caught by the smoke tests.
 		return fmt.Errorf("unknown output format %q", outputFormat)
 	}
 
@@ -130,7 +157,6 @@ var (
 	noColor      bool
 	outputFormat string
 	printVersion bool
-	quiet        bool
 	skipInit     bool
 	skipRefresh  bool
 	terraformBin string
@@ -140,9 +166,8 @@ var (
 func parseFlags() {
 	flag.StringSliceVar(&ignoreRules, "ignore", nil, "ignore differences based on a `rule`")
 	flag.BoolVar(&noColor, "no-color", false, "disable color in output")
-	flag.StringVarP(&outputFormat, "output", "o", "blocks", "output `format` of moves (\"blocks\" or \"commands\")")
+	flag.StringVarP(&outputFormat, "output", "o", "auto", "output `format` of moves (\"auto\", \"blocks\" or \"commands\")")
 	flag.BoolVarP(&printVersion, "version", "V", false, "print version and exit")
-	flag.BoolVarP(&quiet, "quiet", "q", false, "suppress all human-readable output")
 	flag.BoolVarP(&skipInit, "skip-init", "s", false, "skip running terraform init")
 	flag.BoolVarP(&skipRefresh, "skip-refresh", "S", false, "skip running terraform refresh")
 	flag.StringVar(&terraformBin, "terraform-bin", "terraform", "terraform binary to use")
@@ -166,14 +191,30 @@ func engineMovesToTerraformMoves(moves []engine.Move) []terraform.Move {
 	return terraformMoves
 }
 
-func smokeTests() error {
-	if len(flag.Args()) > 1 && outputFormat == "blocks" {
-		return fmt.Errorf("blocks output format is not supported for multiple modules")
+func smokeTests(ctx context.Context) error {
+	if outputFormat != "auto" && outputFormat != "blocks" && outputFormat != "commands" {
+		return fmt.Errorf("unknown output format %q", outputFormat)
 	}
+
+	if outputFormat == "blocks" {
+		if len(flag.Args()) > 1 {
+			return fmt.Errorf("blocks output format is not supported for multiple modules")
+		}
+
+		tfVersion, err := terraform.GetVersion(ctx)
+		if err != nil {
+			return fmt.Errorf("failed to get Terraform version: %w", err)
+		}
+
+		if tfVersion.LessThan(version.Must(version.NewSemver("1.1.0"))) {
+			return fmt.Errorf("Terraform version %s does not support moved blocks", tfVersion)
+		}
+	}
+
 	return nil
 }
 
-func getPlans(ctx context.Context, workdirs []string, options []terraform.PlanOption) ([]engine.Plan, error) {
+func getPlans(ctx context.Context, workdirs []string, options []terraform.Option) ([]engine.Plan, error) {
 	type result struct {
 		plan engine.Plan
 		err  error
@@ -181,15 +222,25 @@ func getPlans(ctx context.Context, workdirs []string, options []terraform.PlanOp
 	results := make([]result, len(workdirs))
 
 	getPlan := func(i int) {
-		jsonPlan, err := terraform.GetPlan(ctx, workdirs[i], options...)
+		workdir := workdirs[i]
+
+		os.Stderr.WriteString(pretty.Colorf("getting Terraform plan for [bold]%s[reset]...", workdir))
+		os.Stderr.WriteString("\n")
+
+		workdirOptions := append(
+			[]terraform.Option{terraform.WithWorkdir(workdir)},
+			options...,
+		)
+
+		jsonPlan, err := terraform.GetPlan(ctx, workdirOptions...)
 		if err != nil {
-			results[i].err = fmt.Errorf("failed to get plan for workdir %q: %w", workdirs[i], err)
+			results[i].err = fmt.Errorf("failed to get plan for workdir %q: %w", workdir, err)
 			return
 		}
 
 		plan, err := engine.SummarizeJSONPlan(workdirs[i], jsonPlan)
 		if err != nil {
-			results[i].err = fmt.Errorf("failed to summarize plan for workdir %q: %w", workdirs[i], err)
+			results[i].err = fmt.Errorf("failed to summarize plan for workdir %q: %w", workdir, err)
 			return
 		}
 
@@ -223,4 +274,54 @@ func getPlans(ctx context.Context, workdirs []string, options []terraform.PlanOp
 	}
 
 	return plans, nil
+}
+
+func categorizeMoves(moves []terraform.Move) (sameWorkdir, differentWorkdir []terraform.Move) {
+	for _, m := range moves {
+		if m.FromWorkdir == m.ToWorkdir {
+			sameWorkdir = append(sameWorkdir, m)
+		} else {
+			differentWorkdir = append(differentWorkdir, m)
+		}
+	}
+
+	return sameWorkdir, differentWorkdir
+}
+
+func writeMovedBlocks(moves []terraform.Move) error {
+	if len(moves) == 0 {
+		return nil
+	}
+
+	movesFilePath := filepath.Join(moves[0].FromWorkdir, "moves.tf")
+	movesFile, err := os.OpenFile(movesFilePath, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		return fmt.Errorf("failed to open %q: %w", movesFilePath, err)
+	}
+
+	err = terraform.WriteMovedBlocks(movesFile, moves)
+	if err != nil {
+		return fmt.Errorf("failed to write moved blocks: %w", err)
+	}
+
+	os.Stderr.WriteString(pretty.Colorf("%s written to [bold][green]%s", pretty.StyledNumMoves(len(moves)), movesFilePath))
+	os.Stderr.WriteString("\n")
+
+	return nil
+}
+
+func writeMoveCommands(moves []terraform.Move) error {
+	if len(moves) == 0 {
+		return nil
+	}
+
+	err := terraform.WriteMoveCommands(os.Stdout, moves)
+	if err != nil {
+		return fmt.Errorf("failed to write move commands: %w", err)
+	}
+
+	os.Stderr.WriteString(pretty.Colorf("%s written to [bold][green]standard output", pretty.StyledNumMoves(len(moves))))
+	os.Stderr.WriteString("\n")
+
+	return nil
 }
--- pkg/terraform/plan.go
+++ pkg/terraform/plan.go
@@ -4,33 +4,29 @@ import (
 	"context"
 	"fmt"
 	"os"
-	"os/exec"
 
-	"github.com/busser/tfautomv/pkg/pretty"
 	"github.com/hashicorp/terraform-exec/tfexec"
 	tfjson "github.com/hashicorp/terraform-json"
 )
 
 // GetPlan obtains a Terraform plan from the module in the given working
 // directory. It does so by running a series of Terraform commands.
-func GetPlan(ctx context.Context, workdir string, opts ...PlanOption) (*tfjson.Plan, error) {
-	var settings planSettings
+func GetPlan(ctx context.Context, opts ...Option) (*tfjson.Plan, error) {
+	var settings settings
 
 	settings.apply(append(defaultOptions(), opts...))
 
 	err := settings.validate()
 	if err != nil {
-		return nil, fmt.Errorf("invalid plan options: %w", err)
+		return nil, fmt.Errorf("invalid options: %w", err)
 	}
 
-	tf, err := tfexec.NewTerraform(workdir, settings.terraformBin)
+	tf, err := tfexec.NewTerraform(settings.workdir, settings.terraformBin)
 	if err != nil {
 		return nil, fmt.Errorf("failed to create Terraform executor: %w", err)
 	}
 
 	if !settings.skipInit {
-		logCommand(workdir, "terraform init")
-
 		err := tf.Init(ctx)
 		if err != nil {
 			return nil, fmt.Errorf("failed to initialize Terraform: %w", err)
@@ -38,15 +34,11 @@ func GetPlan(ctx context.Context, workdir string, opts ...PlanOption) (*tfjson.P
 	}
 
 	if !settings.skipRefresh {
-		logCommand(workdir, "terraform refresh")
-
 		err := tf.Refresh(ctx)
 		if err != nil {
 			return nil, fmt.Errorf("failed to refresh Terraform state: %w", err)
 		}
 	}
-
-	logCommand(workdir, "terraform plan")
 
 	planFile, err := os.CreateTemp("", "tfautomv.*.plan")
 	if err != nil {
@@ -65,102 +57,4 @@ func GetPlan(ctx context.Context, workdir string, opts ...PlanOption) (*tfjson.P
 	}
 
 	return plan, nil
-}
-
-type planSettings struct {
-	workdir      string
-	terraformBin string
-	skipInit     bool
-	skipRefresh  bool
-}
-
-// An PlanOption configures how Terraform's plan is fetched.
-type PlanOption func(*planSettings)
-
-func defaultOptions() []PlanOption {
-	return []PlanOption{
-		WithWorkdir("."),
-		WithTerraformBin("terraform"),
-	}
-}
-
-// WithWorkdir sets the directory where the Planner will run Terraform commands.
-// If this option is not provided, the Planner runs in the current working
-// directory.
-func WithWorkdir(workdir string) PlanOption {
-	return func(s *planSettings) {
-		s.workdir = workdir
-	}
-}
-
-// WithTerraformBin overrides the `terraform` binary that the Planner uses.
-// If this option is not provided, the Planner uses the `terraform` binary it
-// finds in the PATH.
-func WithTerraformBin(path string) PlanOption {
-	return func(s *planSettings) {
-		s.terraformBin = path
-	}
-}
-
-// WithSkipInit configures whether the Planner skips initializing the module
-// before obtaining a plan from Terraform. By default, the Planner does not skip
-// this step.
-//
-// Skipping the init step can save time, but subsequent steps may fail if the
-// module was not initialized before using the Planner.
-func WithSkipInit(skipInit bool) PlanOption {
-	return func(s *planSettings) {
-		s.skipInit = skipInit
-	}
-}
-
-// WithSkipRefresh configures whether the Planner skips refreshing the module's
-// state before obtaining a plan from Terraform. By default, the Planner does
-// not skip this step.
-//
-// Skipping the refresh step can save time, but can result in Terraform basing
-// its plan on stale data.
-func WithSkipRefresh(skipRefresh bool) PlanOption {
-	return func(s *planSettings) {
-		s.skipRefresh = skipRefresh
-	}
-}
-
-func (s *planSettings) apply(opts []PlanOption) {
-	for _, opt := range opts {
-		opt(s)
-	}
-}
-
-func (s *planSettings) validate() error {
-	if !isDirectory(s.workdir) {
-		return fmt.Errorf("target directory %q not found", s.workdir)
-	}
-
-	if !isInPath(s.terraformBin) {
-		return fmt.Errorf("executable %q not found in PATH", s.terraformBin)
-	}
-
-	return nil
-}
-
-func isDirectory(path string) bool {
-	fileInfo, err := os.Stat(path)
-	if err != nil {
-		return false
-	}
-
-	return fileInfo.IsDir()
-}
-
-func isInPath(bin string) bool {
-	_, err := exec.LookPath(bin)
-	return err == nil
-}
-
-func logCommand(workdir, command string) {
-	if workdir == "." {
-		workdir = "current directory"
-	}
-	os.Stderr.WriteString(pretty.Colorf("running [bold]%s[reset] in [bold]%s[reset]...", command, workdir) + "\n")
 }
